7e794a18963707ea188d053c75e2a7212f026a7b
Daniel Lopez <daniel.l1.g@hotmail.com>
2023-05-20 14:25:13 -0500

    deploidcomplete

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,15 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  const allowedOrigins = [
+   `https://danielpivideogames.netlify.app/`,
+    'http://localhost:3000' // Agrega la segunda página permitida aquí
+  ]
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
